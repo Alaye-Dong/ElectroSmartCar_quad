f42899e492e854c71df823c2a0a9a93fc44250b2
Alaye-Dong <2971731849@qq.com>
2023-06-26 16:33:18 +0800

    解散KeystrokeLabel_Switch功能，试验菜单11，12

--- Seekfree_STC32G12K128_Opensource_Library/Project/MDK/eeprom.c
+++ Seekfree_STC32G12K128_Opensource_Library/Project/MDK/eeprom.c
@@ -7,25 +7,27 @@ void eeprom_init()
 
     iap_read_bytes(0x00, date_buff, 100);		//从EEPROM中读取数据
 
-/****不用eeprom直接赋值****/
-    g_dirControl_P		= 16.7;//read_float(0);
-    g_dirControl_D    = 15;//read_float(1);
-	  angle_kp1     		= 0.425;//read_float(2);
+///****不用eeprom直接赋值****/
+//    g_dirControl_P		= 16.7;//read_float(0);
+//    g_dirControl_D    = 15;//read_float(1);
+//	  angle_kp1     		= 0.425;//read_float(2);
 
-    Motor_Kp_L 			  = 45;//read_float(3);
-    Motor_Ki_L      	= 5.7;//read_float(4);
-	
-    Motor_Kp_R      	= 45;//read_float(5);
-    Motor_Ki_R     		= 5.7;//read_float(6);
+//    Motor_Kp_L 			  = 45;//read_float(3);
+//    Motor_Ki_L      	= 5.7;//read_float(4);
+//	
+//    Motor_Kp_R      	= 45;//read_float(5);
+//    Motor_Ki_R     		= 5.7;//read_float(6);
 	
 /****用eeprom赋值****/	
-//		g_dirControl_P		= read_float(0);
-//		g_dirControl_D    = read_float(1);
-//		angle_kp1     		= read_float(2);
-//    Motor_Kp_L 			  = read_float(3);
-//    Motor_Ki_L      	= read_float(4);
-//    Motor_Kp_R      	= read_float(5);
-//    Motor_Ki_R     		= read_float(6);
+		Motor_Kp_L 			= read_float(0);
+		Motor_Ki_L      	= read_float(1);
+		Motor_Kp_R      	= read_float(2);
+		Motor_Ki_R     	= read_float(3);
+		angle_kp1     		= read_float(4);
+		ZX_kp						= read_float(5);
+		ZX_kd						= read_float(6);
+		g_dirControl_P	= read_float(7);
+		g_dirControl_D   = read_float(8);
 	
 }
 void save_int(int32 input, uint8 value_bit)
